73b4244595df49e00d057aa70f3b94fe53f1f6f4
non <mail@non117.com>
2017-12-28 22:15:21 +0900

    create react app

---
+++ src/component/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default class App extends React.PureComponent {
+  render(): React.ReactNode {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
---
+++ src/renderer.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import App from "./component/App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
