cd033b45b254d279b10c5f35bea6c95e1d5c1140
non <mail@non117.com>
2018-01-01 14:31:52 +0900

    implement App component

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,9 +1,36 @@
 import * as React from "react";
 
-export default class App extends React.PureComponent {
+import ReceiptImage, { ReceiptImageProps } from "./ReceiptImage";
+import AccountSelector, { AccountSelectorProps } from "./AccountSelector";
+import DateSelector, { DateSelectorProps } from "./DateSelector";
+import ItemTable, { ItemTableProps } from "./ItemTable";
+
+export interface AppProps {
+  image: ReceiptImageProps;
+  account: AccountSelectorProps;
+  date: DateSelectorProps;
+  items: ItemTableProps;
+}
+
+export default class App extends React.PureComponent<AppProps, {}> {
   render(): React.ReactNode {
+    const { image, account, date, items } = this.props;
     return (
       <div>
+        <section id="receipt">
+          <ReceiptImage {...image} />
+        </section>
+        <section id="input">
+          <AccountSelector {...account} />
+          <DateSelector {...date} />
+          <div>
+            <button>🔃</button>
+          </div>
+          <ItemTable {...items} />
+          <div>
+            <button id="register">登録</button>
+          </div>
+        </section>
       </div>
     );
   }
