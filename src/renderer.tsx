fb3bf2b80661652a6c7bcd00f0ce836c4738e152
non <mail@non117.com>
2018-01-04 15:12:08 +0900

    test data for primitive component

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,4 +2,17 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import App from "./component/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const exampleState = {
+  "image": { "imagePath": "/Users/non/Desktop/receipt/IMG_0001.jpg" },
+  "account": { "accounts": ["hoge"] },
+  "date": { "date": "2018-01-01" },
+  "items": {
+    "items": [
+      { "name": "a", "price": 1000, "account": "" },
+      { "name": "b", "price": 1000, "account": "" }
+    ],
+    "sum": 2000
+  }
+};
+
+ReactDOM.render(<App {...exampleState} />, document.getElementById("app"));
