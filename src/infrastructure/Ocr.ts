d6866063e3f01e531d538f5401bcced829280baf
non <mail@non117.com>
2018-01-06 23:46:53 +0900

    refactoring

--- src/infrastructure/Ocr.ts
+++ src/infrastructure/Ocr.ts
@@ -2,37 +2,24 @@ import * as Axios from "axios";
 import { AxiosPromise } from "axios";
 import { readEncodedImage } from "./ImageReader";
 
-declare module namespace {
-  // request structure
-  interface Feature {
-    type: string;
-    maxResults: number;
-  }
-  interface Image {
-    content: string;
-  }
-  interface Request {
-    image: Image;
-    features: Feature[];
-  }
-  // response structure
-  interface Vertice {
-    x: number;
-    y: number;
-  }
-  interface BoundingPoly {
-    vertices: Vertice[];
-  }
-  interface TextAnnotation {
-    locale: string | null;
-    description: string;
-    boundingPoly: BoundingPoly;
-  }
-  interface Response {
-    textAnnotations: TextAnnotation[];
-  }
+// response structure
+interface Vertice {
+  x: number;
+  y: number;
 }
-export class Client {
+interface BoundingPoly {
+  vertices: Vertice[];
+}
+interface TextAnnotation {
+  locale: string | null;
+  description: string;
+  boundingPoly: BoundingPoly;
+}
+interface Response {
+  textAnnotations: TextAnnotation[];
+}
+
+class Client {
   axios: Axios.AxiosInstance;
   annotatePath: "/v1/images:annotate";
   constructor(apiKey: string) {
@@ -41,7 +28,6 @@ export class Client {
       "headers": { "Content-Type": "application/json" }
     });
   }
-  // TODO: when promise is resolved?
   annotate(imagePath: string): AxiosPromise {
     const b64Image = readEncodedImage(imagePath);
     const data = {
@@ -60,3 +46,5 @@ export class Client {
     return this.axios.post(this.annotatePath, data);
   }
 }
+
+export { Vertice, BoundingPoly, TextAnnotation, Response, Client}
