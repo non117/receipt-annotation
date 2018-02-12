5d01ef417a739e2a4e88dcf05b56002ff9d6c41a
non <mail@non117.com>
2018-02-12 23:44:17 +0900

    refactor deserialize process

--- src/domain/Receipt.ts
+++ src/domain/Receipt.ts
@@ -1,24 +1,9 @@
 import Account from "./Account";
 
-interface ReceiptJSON {
-  image_path: string;
-  date: string;
-  sum: number;
-  shop_name: string;
-}
-
 export default class Receipt {
   imagePath: string;
   date: string;
   sum: number;
   shopName: string;
   account: Account;
-
-  static fromJSON(receiptJson: ReceiptJSON): Receipt {
-    let receipt = Object.create(Receipt.prototype);
-    return Object.assign(receipt, receiptJson, {
-      imagePath: receiptJson.image_path,
-      shopName: receiptJson.shop_name,
-    });
-  }
 }
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -3,17 +3,11 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 
 import App from "./component/App";
-import Receipt from "./domain/Receipt";
+import ReceiptDeserializer from "./infrastructure/ReceiptDeserializer";
 
 const jsonPath = "/Users/non/Desktop/output/receipts.json";
 
 const rawJson = fs.readFileSync(jsonPath).toString();
-const receipts = JSON.parse(rawJson, (key: string, value: any) => {
-  if (value instanceof Array) {
-    return value.map(Receipt.fromJSON);
-  } else {
-    return value;
-  }
-});
+const receipts = ReceiptDeserializer.deserializeFromString(rawJson);
 
 ReactDOM.render(<App {...receipts[0]} />, document.getElementById("app"));
