07a9bcf96515c32a815adc4636d3bf21f4601f38
non <mail@non117.com>
2018-02-18 22:01:56 +0900

    Added ReceiptContainer

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,27 +1,10 @@
 import * as React from "react";
+import ReceiptContainer from "./ReceiptContainer";
 
-import ReceiptImage from "./ReceiptImage";
-import AccountSelector from "./AccountSelector";
-import DateSelector from "./DateSelector";
-
-import Receipt from "../domain/Receipt";
-
-export default class App extends React.PureComponent<Receipt, {}> {
+export default class App extends React.PureComponent<{}, {}> {
   render(): React.ReactNode {
-    const { imagePath, account, date, sum } = this.props;
     return (
       <div>
-        <section id="receipt">
-          <ReceiptImage imagePath={imagePath} />
-        </section>
-        <section id="input">
-          <AccountSelector accounts={[]} />
-          <DateSelector date={date} />
-          <input type="text" defaultValue={String(sum)} />
-          <div>
-            <button id="register">登録</button>
-          </div>
-        </section>
       </div>
     );
   }
---
+++ src/component/ReceiptContainer.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import ReceiptImage from "./ReceiptImage";
+import AccountSelector from "./AccountSelector";
+import DateSelector from "./DateSelector";
+
+import Receipt from "../domain/Receipt";
+
+export default class ReceiptContainer extends React.PureComponent<Receipt, {}> {
+  render(): React.ReactNode {
+    const { imagePath, account, date, sum } = this.props;
+    return (
+      <div>
+        <section id="receipt">
+          <ReceiptImage imagePath={imagePath} />
+        </section>
+        <section id="input">
+          <AccountSelector accounts={[]} />
+          <DateSelector date={date} />
+          <input type="text" defaultValue={String(sum)} />
+          <div>
+            <button id="register">登録</button>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
