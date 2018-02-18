4b9b524936dabaaa095d45f42377b8105389dd78
non <mail@non117.com>
2018-02-18 22:26:29 +0900

    prepare receipts

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,7 +2,9 @@ import * as fs from "fs";
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 import App from "./component/App";
+import { LoadReceiptFactory } from "./usecase/LoadReceipt";
 
 const jsonPath = "/Users/non/Desktop/output/receipts.json";
+LoadReceiptFactory.create().execute(jsonPath);
 
 ReactDOM.render(<App {...receipts[0]} />, document.getElementById("app"));
