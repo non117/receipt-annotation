1f4e20eb2fcf986a39c6e87a4ff4d3e8c088d8a4
non <mail@non117.com>
2018-02-22 23:15:53 +0900

    next, prev action

---
+++ src/usecase/MoveNext.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+import ReceiptList from "../domain/ReceiptList";
+import receiptRepository, { ReceiptRepository } from "../infrastructure/ReceiptRepository";
+
+export class MoveNextFactory {
+  static create() {
+    return new MoveNext(receiptRepository);
+  }
+}
+
+export class MoveNext {
+  private receiptRepository: ReceiptRepository;
+
+  constructor(repository: ReceiptRepository) {
+    this.receiptRepository = repository;
+  }
+
+  execute() {
+    const receiptList = receiptRepository.get();
+    receiptList.moveNext();
+    receiptRepository.set(receiptList);
+  }
+}
---
+++ src/usecase/MovePrev.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+import ReceiptList from "../domain/ReceiptList";
+import receiptRepository, { ReceiptRepository } from "../infrastructure/ReceiptRepository";
+
+export class MovePrevFactory {
+  static create() {
+    return new MovePrev(receiptRepository);
+  }
+}
+
+export class MovePrev {
+  private receiptRepository: ReceiptRepository;
+
+  constructor(repository: ReceiptRepository) {
+    this.receiptRepository = repository;
+  }
+
+  execute() {
+    const receiptList = receiptRepository.get();
+    receiptList.movePrev();
+    receiptRepository.set(receiptList);
+  }
+}
