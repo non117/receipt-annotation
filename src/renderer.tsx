7639c08573fd5221912b48c0a95480d733d6dddb
non <mail@non117.com>
2018-02-22 23:16:32 +0900

    refactoring

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,11 +1,16 @@
 import * as fs from "fs";
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-import App from "./component/App";
+import ReceiptContainer from "./component/ReceiptContainer";
 import { LoadReceiptFactory } from "./usecase/LoadReceipt";
 import receiptRepository from "./infrastructure/ReceiptRepository";
 
-receiptRepository.onChange( () => ReactDOM.render(<App />, document.getElementById("app")) );
+receiptRepository.onChange( () => { 
+  const receipt = receiptRepository.get().getCurrent();
+  ReactDOM.render(
+    <ReceiptContainer receipt={receipt} />, document.getElementById("app")
+  );
+});
 
 const jsonPath = "/Users/non/Desktop/output/receipts.json";
 LoadReceiptFactory.create().execute(jsonPath);
