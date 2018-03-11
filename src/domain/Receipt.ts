6536968bc4c61131620fe14569d7b2b1f62a2dd1
non <mail@non117.com>
2018-03-11 23:01:16 +0900

    bugfix

--- src/domain/Receipt.ts
+++ src/domain/Receipt.ts
@@ -23,7 +23,7 @@ export default class Receipt {
     this.imagePath = receipt.imagePath;
     this.date = receipt.date ? moment(receipt.date, Receipt.DATE_FORMAT) : moment();
     this.sum = receipt.sum || 0;
-    this.memo = receipt.memo;
+    this.memo = receipt.memo || "";
     this.debitAccount = receipt.debitAccount;
     this.creditAccount = receipt.creditAccount;
   }
