2d76bd8b3f0826a4d468eb3ca405ab6864c6bcaf
non <mail@non117.com>
2018-06-30 16:19:05 +0900

    accounts belongs to wallet

--- src/domain/Wallet.ts
+++ src/domain/Wallet.ts
@@ -1,17 +1,20 @@
+import { AccountObject } from "./Account";
 import AccountList from "./AccountList";
 
-interface WalletObject {
-  accountList: AccountList;
+export interface WalletObject {
+  name: string;
   defaultCreditAccount: string;
   defaultDebitAccount: string;
   keywords: { [key: string]: string };
+  accounts: Array<AccountObject>;
 }
 
 export default class Wallet {
-  accountList: AccountList;
+  name: string;
   defaultCreditAccount: string;
   defaultDebitAccount: string;
   keywords: { [key: string]: string };
+  accountList: AccountList;
 
   constructor(wallet: WalletObject) {
     Object.assign(this, wallet);
