304bc4d43c2705b759c179292f25312a935a2797
non <mail@non117.com>
2018-08-04 20:23:37 +0900

    Construct Phrase and Line from OCR API response.

--- src/domain/Phrase.ts
+++ src/domain/Phrase.ts
@@ -5,18 +5,9 @@ export default class Phrase {
   x: number;
   text: string;
 
-  constructor(textAnnotation: TextAnnotation) {
-    this.text = textAnnotation.description;
-    const rect = textAnnotation.boundingPoly.vertices;
-    this.height = rect[3].y - rect[0].y;
-    this.width = 0;
-    // ¥単体が入ってくると0 divisionになってしまうので除く
-    if(this.text !== "¥") {
-      const characterCount = this.text.length; // FIXME: ¥をのぞく
-      this.width = (rect[1].x - rect[0].x) / characterCount;
-    }
-    this.position = (rect[3].y + rect[0].y) / 2;
-    this.x = rect.map(p => p.x).sort()[0];
+  constructor(height: number, width: number, position: number, x: number, text: string) {
+    this.height, this.width, this.position, this.x = height, width, position, x;
+    this.text = text;
   }
 
   // 前後の文字列の位置誤差が、フォントの高さに収まっているなら同じ行とする
@@ -25,18 +16,4 @@ export default class Phrase {
     const minHeight = [this.height, otherPhrase.height].sort[0];
     return Math.abs(otherPhrase.position - this.position) < minHeight / 3;
   }
-}
-
-export interface Point {
-  x: number;
-  y: number;
-}
-
-export interface BoundingPoly {
-  vertices: Array<Point>;
-}
-
-export interface TextAnnotation {
-  description: string;
-  boundingPoly: BoundingPoly;
 }
---
+++ src/usecase/services/ParseOcrResponse.ts
@@ -0,0 +1,65 @@
+import Line from "../../domain/Line";
+import Phrase from "../../domain/Phrase";
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface BoundingPoly {
+  vertices: Array<Point>;
+}
+
+interface TextAnnotation {
+  description: string;
+  boundingPoly: BoundingPoly;
+}
+
+interface Response {
+  textAnnotations: Array<TextAnnotation>;
+}
+
+interface AnnotatedText {
+  responses: Array<Response>;
+}
+
+function buildPhrase(textAnnotation: TextAnnotation) {
+  const rect = textAnnotation.boundingPoly.vertices;
+  const height = rect[3].y - rect[0].y;
+  let width = 0;
+  // ¥単体が入ってくると0 divisionになってしまうので除く
+  if (this.text !== "¥") {
+    const characterCount = this.text.length; // FIXME: ¥をのぞく
+    width = (rect[1].x - rect[0].x) / characterCount;
+  }
+  const position = (rect[3].y + rect[0].y) / 2;
+  const x = rect.map(p => p.x).sort()[0];
+  return new Phrase(height, width, position, x, textAnnotation.description);
+}
+
+// 自分のTextと同じ行と思われるTextを全探索し,
+// あとからuniqをかける.
+// OCR結果が行頭からの順序になってるとは限らないため.
+function buildLines(textAnnotations: Array<TextAnnotation>): Array < Line > {
+  const phrases = textAnnotations.map(buildPhrase);
+  const lines = phrases.map(phrase => {
+    let tempPhrases = phrases.filter(otherPhrase => {
+      return phrase !== otherPhrase && phrase.isSameLine(otherPhrase);
+    })
+    tempPhrases.push(phrase);
+    // FIXME: もっとこう手心というか
+    return new Line(tempPhrases.sort((a, b) => {
+      if (a.x < b.x) return -1;
+      if (a.x > b.x) return 1;
+      return 0;
+    }));
+  })
+  return Array.from(new Set(lines));
+}
+
+export function parseOcrResponse(rawOcrResponse: string): Array<Line> {
+  const responseObject: AnnotatedText = JSON.parse(rawOcrResponse); // FIXME: 本当に動くのかこれ
+  const textAnnotations = responseObject[0].textAnnotations;
+  textAnnotations.shift; // 0番目はすべてが結合されたやつなので捨てる
+  return buildLines(textAnnotations);
+}
