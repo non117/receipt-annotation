290b05e60540ada027933cf5607eabb2233158a9
non <mail@non117.com>
2018-08-18 17:52:10 +0900

    walk directory

--- src/infrastructure/FileIO.ts
+++ src/infrastructure/FileIO.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 // https://grizzlybit.info/2017/09/29/Node-JS-8-Util-Promisify/
 import * as util from "util";
 require('util.promisify').shim();
@@ -6,6 +7,13 @@ require('util.promisify').shim();
 export function readFile(path: string): string {
   return fs.readFileSync(path).toString();
   // TODO error handling
+}
+
+export function listFiles(directory: string): Array<string> {
+  const basePath = path.resolve(directory);
+  const files = fs.readdirSync(basePath);
+  // TODO: extensionでフィルタ
+  return files.map(file => path.join(basePath, file));
 }
 
 export function writeFile(path: string, content: string): Promise<void> {
