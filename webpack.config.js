d5da7bc1e3fd96b42ab0fc06047cfba4b69bf259
non <mail@non117.com>
2019-11-02 20:00:18 +0900

    update environment settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
-module.exports = {
-  target: 'electron-renderer',
-  entry: ['./src/renderer.tsx'],
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+const path = require('path')
+
+module.exports = [
+  {
+    target: "electron-renderer",
+    entry: "./src/renderer.tsx",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: "renderer.js"
+    },
+    module: {
+      rules: [
+        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+        { test: /\.ts$/, use: "ts-loader" },
+        { test: /\.tsx$/, use: "ts-loader" }
+      ]
+    }
   },
-  output: {
-    filename: 'renderer.js'
-  },
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.ts$/, use: 'ts-loader' },
-      { test: /\.tsx$/, use: 'ts-loader' }
-    ]
+  {
+    target: "electron-main",
+    entry: "./src/main.ts",
+    resolve: {
+      extensions: [".ts"]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: "main.js"
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, use: "ts-loader" },
+      ]
+    }
   }
-};
+];
